5f7bf2ff6e444cdeb6785bd66c6701cd43a82875
nqt <qitong.niu@gmail.com>
2023-02-06 12:27:40 +0800

    dev

--- main.cpp
+++ main.cpp
@@ -59,27 +59,77 @@ int main(int argc, char* argv[]) {
     http_connection* users = new http_connection[MAX_FD];
 
     int listen_fd = socket(PF_INET, SOCK_STREAM, 0);
-    // if (listen_fd == -1)
+    /*  
+        int socket(int domain, int type, int protocol);
+        socket()创建一个通信端点, 返回指向该端点的文件描述符, 该文件描述符是当前进程可用的值最小的文件描述符
+        socket()  creates  an  endpoint  for communication and returns a file descriptor that refers to that endpoint.
+        The file descriptor returned by a successful call will be the lowest-numbered file  descriptor  not  currently
+        open for the process
+    */
 
     // 设置端口复用 
     int reuse = 1;
     setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-
+    /*  
+        int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
+        When  manipulating  socket options, the level at which the option resides and the name of
+        the option must be specified.  To manipulate options at the sockets API level,  level  is
+        specified as SOL_SOCKET
+        The arguments optval and optlen are used to access option values for  setsockopt() 
+    */
     // bind
     struct sockaddr_in address; 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
+    address.sin_family = AF_INET; // 2 bytes
+    address.sin_addr.s_addr = INADDR_ANY; // 4 bytes
+    address.sin_port = htons(port); // 2 bytes
+    // padding
+
+    /*     
+        struct sockaddr_in
+        {
+            __SOCKADDR_COMMON (sin_);
+            in_port_t sin_port;			/* Port number.  
+            struct in_addr sin_addr;		/* Internet address.  
+
+            /* Pad to size of `struct sockaddr'.  
+            unsigned char sin_zero[sizeof (struct sockaddr) -
+                    __SOCKADDR_COMMON_SIZE -
+                    sizeof (in_port_t) -
+                    sizeof (struct in_addr)];
+        };
+    */
     int ret = 0;
     ret = bind(listen_fd, (struct sockaddr*)&address, sizeof(address));
 
     // listen
     ret = listen(listen_fd, 5);
 
+    /*
+        typedef union epoll_data
+        {
+        void *ptr;
+        int fd;
+        uint32_t u32;
+        uint64_t u64;
+        } epoll_data_t;
+
+        struct epoll_event
+        {
+        uint32_t events;	/* Epoll events 
+        epoll_data_t data;	/* User data variable 
+        } __EPOLL_PACKED;
+    */
     // IO multiplexing, epoll(), events array
     epoll_event events[MAX_EVENT_NUMBER];
     int epoll_fd = epoll_create(5);
-
+    /*
+        返回一个指向epoll instance的文件描述符, 用于后续所有对该epoll的调用
+        当指向一个epoll instance的文件描述符全部被close()之后, 该epoll instance就会被释放
+        epoll_create() returns a file descriptor referring to the new epoll instance.  This file  descriptor  is  used
+        for all the subsequent calls to the epoll interface.  When no longer required, the file descriptor returned by
+        epoll_create() should be closed by using close(2).  When all file descriptors referring to an  epoll  instance
+        have been closed, the kernel destroys the instance and releases the associated resources for reuse.
+    */
     addfd(epoll_fd, listen_fd, false);
 
     http_connection::m_epoll_fd = epoll_fd;
